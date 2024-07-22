59bc26da80e667887701455f83a5e09d04a244d4
ge65mil <ge65mil@tum.de>
2024-07-22 11:45:36 +0200

    Improved moving average for plotting of runs

--- visualizations/create_training_plots.py
+++ visualizations/create_training_plots.py
@@ -7,16 +7,25 @@ ALGO = "TQC_1"
 WINDOW_SIZE = 1
 
 def moving_average(data, window_size):
-    """Apply moving average filter to data."""
+    """Apply centered moving average filter to data with padding."""
+    if window_size % 2 == 0:
+        raise ValueError("Window size must be odd to center the window.")
+    
+    pad_width = window_size // 2
+    padded_data = np.concatenate((np.zeros(pad_width), data.to_numpy()))
+    
     weights = np.repeat(1.0, window_size) / window_size
-    return np.convolve(data, weights, 'valid')
+    return np.convolve(padded_data, weights, 'valid')
 
 # Load CSV file into a pandas DataFrame
 df = pd.read_csv(f'/root/drl_grasping_training/train/{ENV}/{ALGO}_tensorboard_training_data.csv')
 
 smoothed_mean_reward = moving_average(df['eval/mean_reward'], WINDOW_SIZE)
 smoothed_success_rate = moving_average(df['eval/success_rate'], WINDOW_SIZE)
-smoothed_steps = df['Step'].iloc[WINDOW_SIZE-1:]
+if WINDOW_SIZE != 1:
+    smoothed_steps = df['Step'].iloc[:int(-(WINDOW_SIZE-1)/2)]
+else:
+    smoothed_steps = df['Step']
 
 # Plot 1: 'eval/mean_reward' vs 'Step'
 plt.figure(figsize=(10, 6))
