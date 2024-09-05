f4f78c760d32bc33f7c6b096c13fc4895e2de7fd
ge65mil <ge65mil@tum.de>
2024-09-05 12:21:38 +0200

    ex_random_agent.bash

--- examples/ex_random_agent.bash
+++ examples/ex_random_agent.bash
@@ -14,14 +14,14 @@ ROBOT_MODEL="lunalab_summit_xl_gen"
 ## ID of the environment
 ## Reach
 # ENV="Reach-Gazebo-v0"
-# ENV="Reach-ColorImage-Gazebo-v0"
+ ENV="Reach-ColorImage-Gazebo-v0"
 # ENV="Reach-DepthImage-Gazebo-v0"
 # ENV="Reach-Octree-Gazebo-v0"
 # ENV="Reach-OctreeWithIntensity-Gazebo-v0"
 # ENV="Reach-OctreeWithColor-Gazebo-v0"
 ## Grasp
 # ENV="Grasp-Gazebo-v0"
- ENV="Grasp-Octree-Gazebo-v0"
+# ENV="Grasp-Octree-Gazebo-v0"
  #ENV="Grasp-OctreeWithIntensity-Gazebo-v0"
 # ENV="Grasp-OctreeWithColor-Gazebo-v0"
 ## GraspPlanetary
