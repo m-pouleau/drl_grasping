031cd24ed44b27584af33579aef55fbd14b3c300
Xiangtong Yao <yaox@vmknoll81.in.tum.de>
2024-09-10 12:30:11 +0200

    Fixed nvidia cuda issue inside container (works in headless way now)

--- .docker/apptainer_commands.bash
+++ .docker/apptainer_commands.bash
@@ -2,6 +2,4 @@
 cd /root
 source /root/entrypoint.bash
 
-nvidia-smi
-
-#ros2 run drl_grasping ex_random_agent.bash
+ros2 run drl_grasping ex_train.bash
--- .docker/run_drl_grasping_apptainer.bash
+++ .docker/run_drl_grasping_apptainer.bash
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 pwd; hostname; date
 
+TAG="drl_grasping_apptainer.sif"
+
+# Create all mounted repositories necessary for running container
 mkdir -p /tmp/.ros/log
 mkdir -p /tmp/.pcg
 mkdir -p /tmp/.rviz2
@@ -10,57 +13,11 @@ mkdir -p /tmp/.ignition/gazebo
 mkdir -p /tmp/.ignition/log
 chmod 777 /tmp/.ros/log /tmp/.pcg /tmp/.rviz2 /tmp/.config/matplotlib /tmp/.cache/matplotlib /tmp/.ignition/gazebo /tmp/.ignition/log
 
-TAG="drl_grasping_apptainer.sif"
+# Set to 1 if GPU's should be used 
+export USE_SINGULARITY_NV=1
 
-## Forward custom volumes and environment variables
+## Forward custom volumes variables
 CUSTOM_VOLUMES=()
-CUSTOM_ENVS=()
-while getopts ":v:e:" opt; do
-    case "${opt}" in
-        v) CUSTOM_VOLUMES+=("${OPTARG}") ;;
-        e) CUSTOM_ENVS+=("${OPTARG}") ;;
-        *)
-            echo >&2 "Usage: ${0} [-v VOLUME] [-e ENV] [TAG] [CMD]"
-            exit 2
-            ;;
-    esac
-done
-shift "$((OPTIND - 1))"
-
-## GUI
-# To enable GUI, make sure processes in the container can connect to the x server
-XAUTH=/tmp/.apptainer.xauth
-
-# Always delete /tmp/.apptainer.xauth (regardless of type) and recreate it
-if [ -e ${XAUTH} ]; then
-    rm -rf ${XAUTH}
-fi
-
-touch ${XAUTH}
-chmod a+r ${XAUTH}
-
-XAUTH_LIST=$(xauth nlist "${DISPLAY}")
-if [ -n "${XAUTH_LIST}" ]; then
-    # shellcheck disable=SC2001
-    XAUTH_LIST=$(sed -e 's/^..../ffff/' <<<"${XAUTH_LIST}")
-    echo "${XAUTH_LIST}" | xauth -f ${XAUTH} nmerge -
-fi
-
-# GUI-enabling volumes
-GUI_VOLUMES=(
-    "${XAUTH}:${XAUTH}"
-    "/tmp/.X11-unix:/tmp/.X11-unix"
-    "/dev/input:/dev/input"
-)
-# GUI-enabling environment variables
-GUI_ENVS=(
-    XAUTHORITY="${XAUTH}"
-    QT_X11_NO_MITSHM=1
-    DISPLAY="${DISPLAY}"
-    #LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
-)
-
-## Additional volumes
 # Synchronize timezone with host
 CUSTOM_VOLUMES+=("/etc/localtime:/etc/localtime:ro")
 # Persistent storage of logs
@@ -80,7 +37,8 @@ CUSTOM_VOLUMES+=("${PWD}/hyperparams:/root/ws/src/drl_grasping/hyperparams")
 CUSTOM_VOLUMES+=("${PWD}/rviz:/root/ws/src/drl_grasping/rviz")
 CUSTOM_VOLUMES+=("${PWD}/drl_grasping:/root/ws/src/drl_grasping/drl_grasping")
 
-## Additional environment variables
+## Forward custom environment variables
+CUSTOM_ENVS=()
 # Synchronize ROS_DOMAIN_ID with host
 if [ -n "${ROS_DOMAIN_ID}" ]; then
     CUSTOM_ENVS+=("ROS_DOMAIN_ID=${ROS_DOMAIN_ID}")
@@ -90,11 +48,28 @@ if [ -n "${IGN_PARTITION}" ]; then
     CUSTOM_ENVS+=("IGN_PARTITION=${IGN_PARTITION}")
 fi
 
+## Fix Nvidia GPU Driver usage
+nv=""
+if [ "$USE_SINGULARITY_NV" = "1" ]; then
+	# When running on Ubuntu newer than 20.04 host system, the host glibc is newer than 2.31 and this causes problems
+	# together with the --nv switch because Singularity mounts too much files from the host inside the container.
+	# So, if running on a newer host, we need to build a subset of the list of mounted libraries and exclude those that
+	# are not directly related to the nvidia driver.
+	glibc_version="$(ldd --version | grep -i glibc | head -n1 | grep -o '2\.[0-9]\{2,\}' | head -n1 | cut -d. -f2)"
+	if [[ "$glibc_version" -le 31 ]]; then
+		nv="--nv"
+	else
+		echo "Your system uses glibc newer than 2.31 which is used inside the container. Implementing --nv switch via a workaround. Let's hope it works!"
+		libs="$(singularity --debug exec --nv "$IMAGE" echo test 2>&1 | grep 'addLibsMount.*/lib/' | sed 's#^.*\(/lib/[^ ]*\) .*$#\1#')"
+		libs="$(echo "$libs" | grep -v -e libEGL.so -e libGL.so -e libGLdispatch.so -e libGLX.so -e libOpenCL.so -e libOpenGL.so)"
+		nv="$(echo "$libs" | sed 's/^/-B /g' | tr "\n" " ")"
+		nv="$nv -B /usr/share/glvnd/egl_vendor.d/10_nvidia.json"
+	fi
+fi
+
 DOCKER_RUN_CMD=(
     apptainer exec
-    --nv
-    "${GUI_VOLUMES[@]/#/"--bind "}"
-    "${GUI_ENVS[@]/#/"--env "}"
+    $nv
     "${CUSTOM_VOLUMES[@]/#/"--bind "}"
     "${CUSTOM_ENVS[@]/#/"--env "}"
     "$(dirname "${PWD}")/${TAG}"
