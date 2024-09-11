6044beeb0a99ecc8a759f8fe2c6ce49e8bc7786f
Xiangtong Yao <yaox@vmknoll81.in.tum.de>
2024-09-11 18:03:06 +0200

    Steps to fix CUDA memory issue (closed)

--- .docker/apptainer_commands.bash
+++ .docker/apptainer_commands.bash
@@ -2,7 +2,4 @@
 cd /root
 source /root/entrypoint.bash
 
-# Set the environment variable for PyTorch memory management
-export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:256
-
 ros2 run drl_grasping ex_train.bash
