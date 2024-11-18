346a044825a817e9a2b2403747151217b9e138a2
Matthias <matze.1998@gmx.fr>
2024-11-18 10:06:51 +0100

    fixed REACH reward function part 2

--- drl_grasping/envs/tasks/grasp/grasp.py
+++ drl_grasping/envs/tasks/grasp/grasp.py
@@ -17,7 +17,7 @@ from scipy.spatial.transform import Rotation
 from drl_grasping.envs.tasks.curriculums import GraspCurriculum
 from drl_grasping.envs.tasks.manipulation import Manipulation
 from drl_grasping.envs.utils.conversions import orientation_quat_to_6d, quat_to_xyzw
-from drl_grasping.envs.utils.math import get_nearest_point, distance_to_nearest_point, index_of_nearest_point, get_distances_to_point
+from drl_grasping.envs.utils.math import get_nearest_point, get_distances_to_point
 
 
 class Grasp(Manipulation, abc.ABC):
@@ -267,13 +267,11 @@ class Grasp(Manipulation, abc.ABC):
         ee_position=self.get_ee_position()
         object_positions=self.get_object_positions()
 
-        # Get nearest distance to gripper center
-        nearest_object_distance = distance_to_nearest_point(origin=ee_position, points=list(object_positions.values()))
-        # check if distance is under threshold
-        if nearest_object_distance < self.train_curriculum.reach_required_distance:
-            # get indice of nearest point and add it to list of reached objects
-            nearest_object_indice = index_of_nearest_point(origin=ee_position, points=list(object_positions.values()))
-            reached_objects.append(self.object_names[nearest_object_indice])
+        # get distances of objects to gripper center 
+        distance_to_ee = get_distances_to_point(origin=ee_position, points=list(object_positions.values()))
+        # get indices of objects in reaching range and add their names to list of reached objects
+        reached_objects_indices = np.where(distance_to_ee < self.train_curriculum.reach_required_distance)[0]
+        reached_objects.extend([self.object_names[idx] for idx in reached_objects_indices])
 
         return reached_objects
 
